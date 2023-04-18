9c6166e4d16d05a66132ce4b26713a781c4c5a13
Ranom <ranom.m.ruiz@gmail.com>
2023-04-17 22:15:11 -0300

    Arrumando

--- src/pages/Ranom.jsx
+++ src/pages/Ranom.jsx
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 export default function Ranom (){
     return(
         <main>
             <h1>O brabo</h1>
         </main>
-        
-=======
-export default function (){
-    return(
-        <h1>O brabo</h1>
->>>>>>> 3eaf5b6 (Implementação)
-=======
-export default function (){
-    return(
-        <h1>O brabo</h1>
->>>>>>> 3eaf5b6d8ae4a04ec78394df1d1583f351445580
-    )
+    )   
 }
