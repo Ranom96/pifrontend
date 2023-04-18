72d8e4e3b3ec08a3afa2889f9493bb12b0023c7d
unknown <2124290038@ACADEMICO.IESB.BR>
2023-04-17 21:52:46 -0300

    Ceviche

---
+++ src/pages/Ceviche.jsx
@@ -0,0 +1,12 @@
+const Ceviche = (props) => {
+
+    return (
+        <>
+            <h1>{props.texto}</h1>
+            <img src="https://conteudo.imguol.com.br/c/entretenimento/d8/2021/05/20/ceviche-peruano-1621533402516_v2_1170x540.jpg" alt="" />
+            <p>Ceviche é uma comida do PERU!</p>
+        </>
+    )
+}
+
+export default Ceviche;
